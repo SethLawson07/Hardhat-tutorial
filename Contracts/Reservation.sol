c15661186f07a6a4c60132757fa02e68cd2153a7
SethLawson07 <latesethlawsonhetchely@gmail.com>
2023-01-07 21:06:07 +0000

    init reservation contract;

---
+++ Contracts/Reservation.sol
@@ -0,0 +1,73 @@
+//SPDX-License-Identifier: UNLICENSED
+import "hardhat/console.sol";
+
+pragma solidity ^0.8.17;
+
+contract Reservation {
+
+    struct Customer {
+        address id;
+        string firstName;
+        string lastName;        
+    }
+
+    Customer [] customers;
+
+    struct Room {
+        string name;
+        uint price;
+    }
+
+    Room [] rooms;
+
+    struct Reservation {
+        address customer;
+        uint32 roomId;
+        uint32 nbDays;
+        uint32 totalPrice;
+        uint timestamp;
+    }
+
+    Reservation [] reservations;
+
+     // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+     // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
+
+    function addRomm(string _name,uint _price) external {
+        require(_name,_price,"False");
+        rooms.push(_name,_price);
+        console.log("True");
+    }    
+
+    function addcustomer(address _id,string _firstName,string _lastName) external {
+        require(_id,_firstName,_lastName,"False");
+        customers.push(_id,_firstName,_lastName);
+        console.log("True");
+
+    }
+
+    function addReservation(address _customer,uint32 _roomId,uint32 _nbDays) external {
+        uint32 memory price;
+        for(i=0;i<rooms.length;i++){
+            if(rooms[i]==_roomId){
+                price=rooms[i].price;
+            }
+        }
+        require(price>0);
+        _customer.transfer(price);
+        reservations.push(_customer,_roomId,_nbDays,price,block.timestamp);
+    }
+
+    function getBalance() external {
+        return msg.value;
+    }
+
+    
+
+
+
+}
+
